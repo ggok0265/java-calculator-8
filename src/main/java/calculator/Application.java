f9aa4edf217075899220f5650853063d755c4e76
Park, Daekyeong <pdk2037@gmail.com>
2025-10-20 18:10:42 +0900

    feat: 문자열을 입력받아 구분하여 합을 반환

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,30 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Application {
+    private static final ArrayList<String> DELIMITERS = new ArrayList<>(Arrays.asList(",", ":"));
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        String regex = String.join("|", DELIMITERS);
+        int[] nums = Arrays.stream(input.split(regex))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        System.out.println("결과 : " + calculate(nums));
+    }
+
+    private static int calculate(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        return sum;
     }
 }
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -24,6 +24,14 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 커스텀_구분자가_없는_케이스() {
+        assertSimpleTest(() -> {
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
