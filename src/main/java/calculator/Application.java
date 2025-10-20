3ab1c6c140dccdf3edabf73bf65c9a95ad12961f
Park, Daekyeong <pdk2037@gmail.com>
2025-10-20 18:15:57 +0900

    feat: 양수가 아닌 수에 대해 에러 발생

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -23,6 +23,9 @@ public class Application {
     private static int calculate(int[] nums) {
         int sum = 0;
         for (int num : nums) {
+            if (num <= 0) {
+                throw new IllegalArgumentException("입력받은 정수가 양수가 아닙니다.");
+            }
             sum += num;
         }
         return sum;
